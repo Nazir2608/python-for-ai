bc07508308d4d80001a161ce510ec2664c1120ef
Md Nazir <mdnazir2608@gmail.com>
2026-04-14 22:04:33 +0530

    learning python

---
+++ loop.py
@@ -0,0 +1,38 @@
+# Loop Examples
+
+print("Hello!")
+print("Hello!")
+print("Hello!")
+print("Hello!")
+print("Hello!")
+
+for i in range(5):
+    print("Hello!")
+
+
+# Print numbers 0 through 4
+for i in range(5):
+    print(i)
+
+
+# Count from 1 to 5
+for i in range(1, 6):
+    print(i)    
+
+name = "Python"
+for char in name:
+    print(char)
+
+
+
+colors = ["red", "blue", "green"]
+for color in colors:
+    print(color)    
+
+
+#While loops
+count = 0
+
+while count < 5:
+    print("Count:", count)
+    count += 1
