e0f0636bc2bd33727c3f9decb0611fff8d576de2
Md Nazir <mdnazir2608@gmail.com>
2026-04-14 21:59:48 +0530

    refactor

---
+++ controlflow.py
@@ -0,0 +1,41 @@
+age=18
+
+if age>=18:
+    print("You are an adult.")
+else:    print("You are a minor.")
+
+temperature = 25
+if temperature>30:
+    print("It's hot outside.")
+elif temperature>20:
+    print("It's warm outside.")
+else:    print("It's cold outside.")
+
+
+score = 85
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+else:    print("Grade: F")  
+
+
+age = 25
+has_license = True
+
+if age >= 18 and has_license:
+    print("You can drive.")
+else:    print("You cannot drive.") 
+
+has_ticket = True
+age = 17
+
+if has_ticket and age >= 18:
+    print("You can enter the concert.")
+else:    print("You cannot enter the concert.")
+
